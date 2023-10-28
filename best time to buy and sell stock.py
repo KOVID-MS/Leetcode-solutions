253255ec7fdc57abe4cbe3e909395559dc0fb14a
KOVID-MS <128456402+KOVID-MS@users.noreply.github.com>
2023-10-28 19:11:26 +0530

    Add files via upload

---
+++ best time to buy and sell stock.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        buy = prices[0]
+        for i in range(len(prices)):
+            if prices[i] > buy:
+                profit = max(profit, prices[i] - buy)
+            else:
+                buy = prices[i]
+        return profit
+
+
+
+
