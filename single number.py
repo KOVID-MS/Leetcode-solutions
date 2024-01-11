6a7c89cf3ff69d177f3e89bf30eaef976414da07
KOVID-MS <128456402+KOVID-MS@users.noreply.github.com>
2024-01-11 20:14:20 +0530

    Add files via upload

---
+++ single number.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution(object):
+    def singleNumber(self, nums):
+        self.nums = nums
+        dictionary = dict(Counter(nums))
+        for key,value in dictionary.items():
+            if value == 1:
+                return key
