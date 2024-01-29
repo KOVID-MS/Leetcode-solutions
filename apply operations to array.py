1cd043fdc8db42852f3bf7600f67db3f97b27246
KOVID-MS <128456402+KOVID-MS@users.noreply.github.com>
2024-01-29 23:07:43 +0530

    Add files via upload

---
+++ apply operations to array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def applyOperations(self, nums):
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] = 2 * nums[i + 1]
+                nums[i + 1] = 0
+        count = nums.count(0)
+        nums = [i for i in nums if i != 0]
+
+        for i in range(count):
+            nums.append(0)
+
+        return nums
