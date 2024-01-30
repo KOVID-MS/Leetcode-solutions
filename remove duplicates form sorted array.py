1216e2fd46ff5adf8a0e233220dda739dfba9c75
KOVID-MS <128456402+KOVID-MS@users.noreply.github.com>
2024-01-30 22:13:09 +0530

    Add files via upload

--- remove duplicates form sorted array.py
+++ remove duplicates form sorted array.py
@@ -1,22 +1,10 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         if not nums:
             return 0
-
         nums2 = []
-        for i in range(len(nums)):
-            if nums[i] not in nums2:
-                nums2.append(nums[i])
-
-        for i in range(len(nums)):
-            if i < len(nums2):
-                nums[i] = nums2[i]
-            else:
-                nums[i] = '_'
-        count = len(nums2)
-
-        return count
+        for num in nums:
+            if num not in nums2:
+                nums2.append(num)
+        nums[:] = nums2
+        return len(nums)
