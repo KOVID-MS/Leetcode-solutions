3919f44cd3e1438a44382e9c514f651f20869825
KOVID-MS <128456402+KOVID-MS@users.noreply.github.com>
2024-07-05 21:39:39 +0530

    Add files via upload

--- valid paranthesis.py
+++ valid paranthesis.py
@@ -1,40 +1,33 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        self.s = s
-        count = 0
-        stack = []
+def isValid(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    self.s = s
+    stack = []
 
-        if len(s) == 1:
-            return False
+    if len(s) == 1:
+        return False
 
-        if len(s) % 2 == 1:
-            return False
+    if len(s) % 2 == 1:
+        return False
 
-        for i in s:
-            if i == '(' or i == '{' or i == '[':
-                stack.append(i)
-            elif i == ')' or i == '}' or i == ']':
-                if len(stack) == 0:
-                    return False
-                p = stack.pop()
-                if i == ')' and p == '(':
-                    count += 1
-                if i == '}' and p == '{':
-                    count += 1
-                if i == ']' and p == '[':
-                    count += 1
+    for i in s:
+        if i == '(' or i == '{' or i == '[':
+            stack.append(i)
+        elif i == ')' or i == '}' or i == ']':
+            if len(stack) == 0:
+                return False
+            p = stack.pop()
+            if i == ')' and p == '(':
+                continue
+            if i == '}' and p == '{':
+                continue
+            if i == ']' and p == '[':
+                continue
+            else:
+                return False
 
-        if count == len(s) // 2:
-            return True
-
-
-
-
-
-
-
-
+    if stack:
+        return False
+    return True
